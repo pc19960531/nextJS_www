2273c6a2cb7b31cd552c68a5c85db859c8b0e08c
pc <pc15161181368>
2018-12-06 11:19:52 +0800

    fix

--- components/Layout.js
+++ components/Layout.js
@@ -40,7 +40,7 @@ export default function layout(Component, isIndex) {
                 } catch (e) {
                     console.log('catch')
                     return { language, agent }
-                } 
+                }
                 // finally {
                 //     console.log('finally')
                 //     return { language, agent }
@@ -72,6 +72,24 @@ export default function layout(Component, isIndex) {
 
         //     }
         // }
+
+        componentDidMount() {
+            let title = '';
+            const res = axios(
+                {
+                    method: "POST",
+                    url: '/1.0/app/web/details',
+                    data: { lang: 'zh-cn', id: '5bd97b86f9463f45327f5345' },
+                    "Content-Type": "application/json",
+                    // timeout: 1000
+                }
+            )
+            if (res.data) {
+                title = res.data.details.title;
+            }
+            console.log('componentDidMount')
+            console.log('title:' + title)
+        }
         render() {
             let { language, agent, title } = this.props;
             console.log('render_title:' + title)
