7cb87ed6f285ffdd5048ccec34399ce7de9f89c5
pc <pc15161181368>
2018-12-06 15:38:15 +0800

    fix

--- components/Layout.js
+++ components/Layout.js
@@ -19,11 +19,8 @@ export default function layout(Component, isIndex) {
             let language = path.indexOf('en-us') === -1 ? zh_cn : en_us;
             let isNews = path.indexOf('newsdetail') !== -1 ? true : false;
             if (isNews) {
-                console.log('进来')
                 let title = '';
                 let alias = path.split('/')[3];
-                console.log('alias:' + alias)
-                console.log('lang:' + language.Lang)
                 try {
                     let opt = {
                         method: "POST",
@@ -35,37 +32,19 @@ export default function layout(Component, isIndex) {
                         credentials: 'same-origin',
                         // timeout: 1000
                     }
-                    const res = await fetch('https://guspc.com/1.0/app/web/details', opt)
-                    console.log('try')
-                    console.log(`Show data fetched. Count:` + JSON.stringify(res));
-                    // if (res.data) {
-                    //     title = res.data.details.title;
-                    // }
+                    const res = await fetch('/1.0/app/web/details', opt)
+                    const data = await res.json();
+                    if (data) {
+                        title = data.details.title;
+                    }
                     return { language, agent, title }
                 } catch (e) {
-                    console.log('e:' + JSON.stringify(e))
                     return { language, agent }
                 }
             } else {
                 return { language, agent }
             }
 
-        }
-
-        async componentDidMount() {
-            let opt = {
-                method: "POST",
-                body: JSON.stringify({ lang: 'zh-cn', alias: 'zhu-wang-token-he-yue-jin-ri-geng-xin' }),
-                headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    "Content-Type": "application/json;charset=UTF-8",
-                },
-                credentials: 'same-origin',
-                // timeout: 1000
-            }
-            const res = await fetch('/1.0/app/web/details', opt)
-            console.log('componentDidMount')
-            console.log(`Show data fetched. Count:` + JSON.stringify(res));
         }
 
         render() {
